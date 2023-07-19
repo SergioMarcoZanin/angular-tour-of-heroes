e76f953af0b13ffc07ccf0ae4ecf2b8908fdf8aa
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2023-07-19 09:41:29 +0200

    moduli http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +13,19 @@ import { MessageService } from './message.service';
 export class HeroService {
   [x: string]: any;
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  private heroesUrl = 'api/heroes'; 
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
+  
   getHero(id: number): Observable<Hero> {
     // Per ora, supponiamo che esista sempre un eroe con l''id' specificato.
     // La gestione degli errori verrà aggiunta nel passaggio successivo del tutorial.
